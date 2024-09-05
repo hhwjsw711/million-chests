0031e72a338b4e3e4b24e1d96eba28d9dfc845c0
huhongwei <381093478@qq.com>
2024-09-05 11:09:01 +0800

    去掉调试信息

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,6 @@ function Chest({
       const currentValue = localStore.getQuery(api.chests.getChestPartition, {
         partition: partitionIndex,
       });
-      console.log("Current partition value:", currentValue);
       if (currentValue) {
         localStore.setQuery(
           api.chests.getChestPartition,
@@ -73,14 +72,8 @@ function Chest({
         onClick={() => {
           openChest({ index, sessionId: SESSION_ID }).then((code) => {
             if (code) {
-              console.log("Chest opened successfully, code:", code);
               data.onCodeFound(code);
-            } else {
-              console.error("Chest opening failed: no code returned");
             }
-          })
-          .catch((error) => {
-            console.error("Error opening chest:", error);
           });
         }}
       >
